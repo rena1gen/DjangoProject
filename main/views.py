38f72484eef3d4eca0ae9a9584d47445de41b4e8
kostarodionov <rodion_rodionov_2015@mail.ru>
2023-05-04 17:34:46 +0400

    aa

--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "mainApp/html/data.html")
+
+def index(request):
+    return render(request, "mainApp/html/data.html")
