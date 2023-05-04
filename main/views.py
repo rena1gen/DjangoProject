72a74236042df7c74c999d38c299804c4a375c14
kostarodionov <rodion_rodionov_2015@mail.ru>
2023-05-04 18:34:36 +0400

    aboutcss

--- main/views.py
+++ main/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "mainApp/html/data.html")
+
 
 def about(request):
     return render(request, "mainApp/html/about.html")
